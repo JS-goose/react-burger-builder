22cae284b413047bd14cbf03449413b768dcee87
Jonathan Sexton <joncsexton@gmail.com>
2019-05-28 05:59:42 -0500

    added renders for Burger and Ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import styles from './Burger.module.css'
+import styles from './Burger.module.css';
+import BurgerIngredients from './Ingredients/BurgerIngredients';
 
 const Burger = (props) => {
-  return <div>
-
+  return <div className={styles['Burger']}>
+      <BurgerIngredients type='bread-top'/>
+      <BurgerIngredients type='bread-cheese'/>
+      <BurgerIngredients type='bread-mean'/>
+      <BurgerIngredients type='bread-bottom'/>
   </div>;
 };
 
