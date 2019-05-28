8c24cf2ed33e363057a4f090f466a383162fc06b
Jonathan Sexton <joncsexton@gmail.com>
2019-05-28 04:50:42 -0500

    Added basic styling to Layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import styles from './Layout.module.css'
 
 const layout = (props) => {
   return (
     <React.Fragment>
       <div>Toolbar, SideDrawer, Backdrop</div>
-      <main>{props.children}</main>
+      <main id={styles['content']}>{props.children}</main>
     </React.Fragment>
   );
 };
