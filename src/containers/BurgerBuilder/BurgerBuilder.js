51eaad686b66c23e23b1eb61c4d9a118bf056417
Jonathan Sexton <joncsexton@.com>
2019-05-28 15:53:33 -0500

    passed state to burger

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,10 +2,18 @@ import React, { Component } from 'react';
 import Burger from '../../components/Burger/Burger'
 
 class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      salad: 1,
+      tomato: 1,
+      cheese: 1,
+      meat: 2,
+    }
+  }
   render() {
     return (
       <React.Fragment>
-        <Burger />
+        <Burger ingredients={this.state.ingredients}/>
         <div>Build Controls</div>
       </React.Fragment>
     );
