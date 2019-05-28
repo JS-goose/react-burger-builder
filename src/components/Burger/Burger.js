ea2985f01a162643938f1aab47f7abc88118a0a2
Jonathan Sexton <joncsexton@gmail.com>
2019-05-28 06:33:25 -0500

    code format change

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,13 +2,15 @@ import React from 'react';
 import styles from './Burger.module.css';
 import BurgerIngredients from './Ingredients/BurgerIngredients';
 
-const Burger = (props) => {
-  return <div className={styles['Burger']}>
-      <BurgerIngredients type='bread-top'/>
-      <BurgerIngredients type='bread-cheese'/>
-      <BurgerIngredients type='bread-mean'/>
-      <BurgerIngredients type='bread-bottom'/>
-  </div>;
+const burger = (props) => {
+  return (
+    <div className={styles['Burger']}>
+      <BurgerIngredients type="bread-top" />
+      <BurgerIngredients type="bread-cheese" />
+      <BurgerIngredients type="bread-mean" />
+      <BurgerIngredients type="bread-bottom" />
+    </div>
+  );
 };
 
-export default Burger;
+export default burger;
