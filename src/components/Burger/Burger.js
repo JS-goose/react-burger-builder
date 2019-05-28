c4acff069eac68d5005e2bf4c7db1a78fb7cae59
Jonathan Sexton <joncsexton@.com>
2019-05-28 16:05:31 -0500

    added a map method for ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,6 +3,11 @@ import styles from './Burger.module.css';
 import BurgerIngredients from './Ingredients/BurgerIngredients';
 
 const burger = (props) => {
+  const transformedIngredients = Object.keys(props.ingredients).map((itemKey) => {
+    return [...Array(props.ingredients[itemKey])].map((_, i) => {
+      return <BurgerIngredients key={itemKey + i} type={itemKey} />
+    })
+  })
   return (
     <div className={styles['Burger']}>
       <BurgerIngredients type="bread-top" />
