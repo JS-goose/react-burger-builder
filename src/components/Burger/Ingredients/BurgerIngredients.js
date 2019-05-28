a18d902d741dc0515a8d4060cd6a02b9a1e1c20f
Jonathan Sexton <joncsexton@gmail.com>
2019-05-28 05:20:14 -0500

    added switch statement for ingredients

---
+++ src/components/Burger/Ingredients/BurgerIngredients.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import styles from './BurgerIngredients.module.css';
+
+const burgerIngredient = (props) => {
+  let ingredient = null;
+
+  switch (props.type) {
+    case 'bread-bottom':
+      ingredient = <div className={styles['BreadBottom']} />;
+      break;
+    case 'bread-top':
+      ingredient = (
+        <div className={styles['BreadTop']}>
+          <div className={styles['Seeds1']} />
+          <div className={styles['Seeds2']} />
+        </div>
+      );
+      break;
+    case 'salad':
+      ingredient = <div className={styles['Salad']} />;
+      break;
+    case 'bacon':
+      ingredient = <div className={styles['Bacon']} />;
+      break;
+    case 'cheese':
+      ingredient = <div className={styles['Cheese']} />;
+      break;
+    case 'meat':
+      ingredient = <div className={styles['Meat']} />;
+      break;
+    default:
+      ingredient = null;
+  }
+};
+
+export default burgerIngredient;
