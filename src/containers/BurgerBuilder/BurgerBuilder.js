cf6e4d927c52feff80f6ad2a4c903ace1234a678
Jonathan Sexton <joncsexton@gmail.com>
2019-05-29 04:49:57 -0500

    imported BuildControls

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import Burger from '../../components/Burger/Burger'
+import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
 class BurgerBuilder extends Component {
   state = {
@@ -8,13 +9,13 @@ class BurgerBuilder extends Component {
       tomato: 0,
       cheese: 0,
       meat: 0,
-    }
-  }
+    },
+  };
   render() {
     return (
       <React.Fragment>
-        <Burger ingredients={this.state.ingredients}/>
-        <div>Build Controls</div>
+        <Burger ingredients={this.state.ingredients} />
+        <BuildControls />
       </React.Fragment>
     );
   }
