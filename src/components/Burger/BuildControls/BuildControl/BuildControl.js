08ef5fcccf6bc2e7472dfda3494259c955ee3281
Jonathan Sexton <joncsexton@gmail.com>
2019-05-29 05:06:27 -0500

    fixed a spelling error

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -4,8 +4,8 @@ import styles from './BuildControl.module.css';
 const buildControl = (props) => {
     return (<div className={styles['BuildControl']}>
         <div className={styles['Label']}>{props.label}</div>
-        <button classname={styles['Less']}>Less</button>
-        <button classname={styles['More']}>More</button>
+        <button className={styles['Less']}>Less</button>
+        <button className={styles['More']}>More</button>
     </div>)
 };
 
