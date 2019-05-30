b24e969b70f683e4c9c2956f4b5f103fa7512026
Jonathan Sexton <joncsexton@gmail.com>
2019-05-30 05:02:14 -0500

    code format update

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -3,19 +3,27 @@ import styles from './BuildControls.module.css';
 import BuildControl from './BuildControl/BuildControl';
 
 const controls = [
-    {label: 'Salad', type: 'salad'},
-    {label: 'Meat', type: 'meat'},
-    {label: 'Cheese', type: 'cheese'},
-    {label: 'Bacon', type: 'bacon'},
-    {label: 'Tomato', type: 'tomato'},
-]
+  { label: 'Salad', type: 'salad' },
+  { label: 'Meat', type: 'meat' },
+  { label: 'Cheese', type: 'cheese' },
+  { label: 'Bacon', type: 'bacon' },
+  { label: 'Tomato', type: 'tomato' },
+];
 
 const buildControls = (props) => (
-    <div className={styles['BuildControls']}>
-        {controls.map(item => {
-            return <BuildControl addIngredient={props.addIngredient} removeIngredient={props.removeIngredient} type={item} key={item.label} label={item.label}/>
-        })}
-    </div>
+  <div className={styles['BuildControls']}>
+    {controls.map((item) => {
+      return (
+        <BuildControl
+          addIngredient={props.addIngredient}
+          removeIngredient={props.removeIngredient}
+          type={item}
+          key={item.label}
+          label={item.label}
+        />
+      );
+    })}
+  </div>
 );
 
 export default buildControls;
