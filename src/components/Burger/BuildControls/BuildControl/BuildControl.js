03758a88402daf765904390254eda30bc08e715e
Jonathan Sexton <joncsexton@gmail.com>
2019-05-30 05:16:48 -0500

    code format update

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -2,11 +2,17 @@ import React from 'react';
 import styles from './BuildControl.module.css';
 
 const buildControl = (props) => {
-    return (<div className={styles['BuildControl']}>
-        <div className={styles['Label']}>{props.label}</div>
-        <button onClick = {props.removeIngredient} className={styles['Less']}>Less</button>
-        <button onClick = {props.addIngredient} className={styles['More']}>More</button>
-    </div>)
+  return (
+    <div className={styles['BuildControl']}>
+      <div className={styles['Label']}>{props.label}</div>
+      <button onClick={props.removeIngredient} className={styles['Less']}>
+        Less
+      </button>
+      <button onClick={props.addIngredient} className={styles['More']}>
+        More
+      </button>
+    </div>
+  );
 };
 
 export default buildControl;
