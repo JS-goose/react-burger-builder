b87e72cac3782ab12619341130cbf1c699967966
Jonathan Sexton <joncsexton@gmail.com>
2019-06-10 05:18:10 -0500

    imported Modal

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+import Modal from '../../components/UI/Modal';
 
 const INGREDIENT_PRICES = {
   salad: 0.3,
