ff2f70a18d6bfdeab2fa77b459c93af12395c72f
Jonathan Sexton <joncsexton@gmail.com>
2019-06-28 06:00:43 -0500

    added dynamic content generated from ingredients to modal

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,13 +1,22 @@
 import React from 'react';
 
 const OrderSummary = (props) => {
+  const summary = Object.keys(props.ingredients).map((ingredient) => {
     return (
-        <React.Fragment>
-            <h3>Your Order</h3>
-            <p>Your tasty burger with the fresh ingredients listed below:</p>
-            <ul></ul>
-        </React.Fragment>
+      <li>
+        {ingredient}: {props.ingredients[ingredient]}
+      </li>
     );
-}
+  });
+  return (
+    <React.Fragment>
+      <h3>Your Order</h3>
+      <p>Your tasty burger with the fresh ingredients listed below:</p>
+      <ul>
+        {summary}
+      </ul>    
+    </React.Fragment>
+  );
+};
 
-export default OrderSummary
+export default OrderSummary;
