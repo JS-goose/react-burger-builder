6ce72cad00e83947bf2d421f6891c823f3d1106d
Jonathan Sexton <joncsexton@gmail.com>
2019-06-28 05:29:08 -0500

    adding content for the burger modal to display

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const OrderSummary = (props) => {
+    return (
+        <React.Fragment>
+            <h3>Your Order</h3>
+            <p>Your tasty burger with the fresh ingredients listed below:</p>
+            <ul></ul>
+        </React.Fragment>
+    );
+}
+
+export default OrderSummary
--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './Modal.module.css'
 
 const Modal = (props) => {
-  return <div id={styles['Modal-container']}>{props.children}a</div>;
+  return <div id={styles['Modal-container']}>{props.children}</div>;
 };
 
 export default Modal;
