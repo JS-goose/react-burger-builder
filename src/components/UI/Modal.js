caed7e08ef2a0832f12ebfcfdb7834e42a37b2da
Jonathan Sexton <joncsexton@gmail.com>
2019-07-03 05:30:32 -0500

    working on hiding modal when not in use

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -2,7 +2,9 @@ import React from 'react';
 import styles from './Modal.module.css'
 
 const Modal = (props) => {
-  return <div id={styles['Modal-container']}>{props.children}</div>;
+  return <div id={styles['Modal-container']}>
+    {props.children}
+  </div>;
 };
 
 export default Modal;
