27753b37af5b405cabece810269f920979e74d95
Jonathan Sexton <joncsexton@.com>
2019-07-04 10:29:14 -0500

    attempting to fix modal display...still

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,18 +1,24 @@
-import React from 'react';
-import styles from './Modal.module.css'
-
-let modalDisplay;
+import React from "react";
+import styles from "./Modal.module.css";
 
 const Modal = (props) => {
-  if (!props.orderable) {
-    return <div className={styles['hidden']}>
-    {props.children}
-  </div>;
-  } else if (props.orderable) {
-    return <div className={styles['Modal-container']}>
-    {props.children}
-  </div>;
-  }
+  // const modalDisplay = <div className={styles["Modal-container"]}>{props.children}</div>;
+
+  // const modalNoDisplay = <div className={styles["Modal-container-hidden"]}>{props.children}</div>;
+
+  // return props.purchaseable ? modalDisplay : modalNoDisplay;
+
+  // if (!props.purchaseable) {
+  //   return <div className={styles['hidden']}>
+  //   {props.children}
+  // </div>;
+  // }
+
+  // if (props.purchaseable) {
+  //   return <div className={styles['Modal-container']}>
+  //   {props.children}
+  // </div>;
+  // }
 };
 
 export default Modal;
