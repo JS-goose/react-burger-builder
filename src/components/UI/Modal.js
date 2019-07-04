547702133f924e9dac92aaf68a4eff8bd44e2682
Jonathan Sexton <joncsexton@.com>
2019-07-04 09:48:03 -0500

    working on an alternate method of showing the modal

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import styles from './Modal.module.css'
 
+let modalDisplay;
+
 const Modal = (props) => {
-  return <div id={styles['Modal-container']}>
+  if (!props.orderable) {
+    return <div className={styles['hidden']}>
     {props.children}
   </div>;
+  } else if (props.orderable) {
+    return <div className={styles['Modal-container']}>
+    {props.children}
+  </div>;
+  }
 };
 
 export default Modal;
