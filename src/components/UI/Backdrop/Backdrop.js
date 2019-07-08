361218038f459012d9a24be30a052fa7fd61f616
Jonathan Sexton <joncsexton@gmail.com>
2019-07-08 18:55:22 -0500

    imported styles into backdrop

--- src/components/UI/Backdrop/Backdrop.js
+++ src/components/UI/Backdrop/Backdrop.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import styles from './Backdrop.module.css';
 
 const Backdrop = (props) => {
-    return (props.show ? <div>Here is the backdrop</div> : null);
+    return (props.show ? <div className={styles['Backdrop-container']}>Here is the backdrop</div> : null);
 }
 
 export default Backdrop
