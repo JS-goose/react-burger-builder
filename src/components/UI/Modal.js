8a99512f5d5f50aef038403c46a057c563d5c4a8
Jonathan Sexton <joncsexton@gmail.com>
2019-07-08 04:45:11 -0500

    removed unused code

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,31 +1,13 @@
-import React from "react";
-import styles from "./Modal.module.css";
+import React from 'react';
+import styles from './Modal.module.css';
 
 const Modal = (props) => {
-  // const modalDisplay = <div className={styles["Modal-container"]}>{props.children}</div>;
-
-  // const modalNoDisplay = <div className={styles["Modal-container-hidden"]}>{props.children}</div>;
-
-  // return props.purchaseable ? modalDisplay : modalNoDisplay;
-
-  // if (!props.purchaseable) {
-  //   return <div className={styles['hidden']}>
-  //   {props.children}
-  // </div>;
-  // }
-
-  // if (props.purchaseable) {
-  //   return <div className={styles['Modal-container']}>
-  //   {props.children}
-  // </div>;
-  // }
-
   return (
     <div
-      className={styles["Modal-container"]}
+      className={styles['Modal-container']}
       style={{
-        transform: props.show ? "translateY(0)" : "translateY(-100vh)",
-        opacity: props.show ? "1" : "0",
+        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+        opacity: props.show ? '1' : '0',
       }}
     >
       {props.children}
