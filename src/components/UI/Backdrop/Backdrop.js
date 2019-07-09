701787e7bc8d5c9d08d09d09198c166f7926dbbe
Jonathan Sexton <joncsexton@gmail.com>
2019-07-08 19:04:05 -0500

    added click handler to display/hide backdrop

--- src/components/UI/Backdrop/Backdrop.js
+++ src/components/UI/Backdrop/Backdrop.js
@@ -2,7 +2,9 @@ import React from 'react';
 import styles from './Backdrop.module.css';
 
 const Backdrop = (props) => {
-    return (props.show ? <div className={styles['Backdrop-container']}>Here is the backdrop</div> : null);
-}
+  return props.show ? (
+    <div className={styles['Backdrop-container']} onClick={props.displayModal}>Here is the backdrop</div>
+  ) : null;
+};
 
-export default Backdrop
+export default Backdrop;
