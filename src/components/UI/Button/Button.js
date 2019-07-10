6145f3e389c6a8203892a819beb55658320988ed
Jonathan Sexton <joncsexton@gmail.com>
2019-07-10 04:35:53 -0500

    updated classname for button

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,5 +1,6 @@
 import React from 'react';
+import styles from './Button.module.css';
 
-const Button = (props) => <button onClick={props.clicked}>{props.children}</button>;
+const Button = (props) => <button className={styles['Button', {styles}[props.btnType]]} onClick={props.clicked}>{props.children}</button>;
 
 export default Button;
