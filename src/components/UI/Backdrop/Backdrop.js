fdd27f52b2a3f2f2c97794f1579825bb01045b31
Jonathan Sexton <joncsexton@gmail.com>
2019-07-10 04:25:39 -0500

    removed unnecessary comment from backdrop

--- src/components/UI/Backdrop/Backdrop.js
+++ src/components/UI/Backdrop/Backdrop.js
@@ -3,7 +3,7 @@ import styles from './Backdrop.module.css';
 
 const Backdrop = (props) => {
   return props.show ? (
-    <div className={styles['Backdrop-container']} onClick={props.displayModal}>Here is the backdrop</div>
+    <div className={styles['Backdrop-container']} onClick={props.displayModal}></div>
   ) : null;
 };
 
