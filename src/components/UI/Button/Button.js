6f6bbd33fa2c00eae961f86e8dbe099342ab420e
Jonathan Sexton <joncsexton@gmail.com>
2019-07-10 04:38:26 -0500

    created button stylesheet and added join to classnames

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import styles from './Button.module.css';
 
-const Button = (props) => <button className={styles['Button', {styles}[props.btnType]]} onClick={props.clicked}>{props.children}</button>;
+const Button = (props) => (
+  <button className={styles['Button', { styles }[props.btnType]].join(' ')} onClick={props.clicked}>
+    {props.children}
+  </button>
+);
 
 export default Button;
