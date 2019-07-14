706ed1b7d262c912294bddf9f867ddec6e50425a
Jonathan Sexton <joncsexton@gmail.com>
2019-07-14 14:33:48 -0500

    added basic styling to the ingredient summary list

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import Button from '../../UI/Button/Button.js'
+import Button from '../../UI/Button/Button.js';
 
 const OrderSummary = (props) => {
   // The props being passed down come in object form, this converts that to an array to map over and display the list items
   const summary = Object.keys(props.ingredients).map((ingredient) => {
     return (
       <li key={ingredient}>
-        {ingredient}: {props.ingredients[ingredient]}
+        <span style={{ textDecoration: 'underline', fontWeight: 700 }}>{ingredient}</span>:{' '}
+        {props.ingredients[ingredient]}
       </li>
     );
   });
-  
+
   return (
     <React.Fragment>
       <h3>Order Summary</h3>
       <p>Your tasty burger with the fresh ingredients listed below:</p>
-      <ul>
-        {summary}
-      </ul> 
-      <p>Total Price: ${props.price.toFixed(2)}</p> 
-      <Button btnType='Continue'clicked={props.continuePurchaseHandler}>Continue</Button>  
-      <Button btnType='Cancel' clicked={props.cancelPurchaseHandler}>Cancel</Button>  
+      <ul>{summary}</ul>
+      <p>Total Price: ${props.price.toFixed(2)}</p>
+      <Button btnType="Continue" clicked={props.continuePurchaseHandler}>
+        Continue
+      </Button>
+      <Button btnType="Cancel" clicked={props.cancelPurchaseHandler}>
+        Cancel
+      </Button>
     </React.Fragment>
   );
 };
