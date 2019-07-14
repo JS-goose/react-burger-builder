87428d7ea932a9876538fe2f3dbc5ec82b6d139b
Jonathan Sexton <joncsexton@gmail.com>
2019-07-14 13:54:43 -0500

    imported Toolbar into Layout and adjusted some margin

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Toolbar from '../Navigation/Toolbar/Toolbar'
 import styles from './Layout.module.css'
 
 const layout = (props) => {
   return (
     <React.Fragment>
-      <div>Toolbar, SideDrawer, Backdrop</div>
+      <Toolbar />
+      <div>SideDrawer</div>
       <main id={styles['content']}>{props.children}</main>
     </React.Fragment>
   );
