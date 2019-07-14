15edbd688038a7c46aa2649d41e9e98512c1209b
Jonathan Sexton <joncsexton@gmail.com>
2019-07-14 13:30:34 -0500

    added basic placeholder content to Toolbar

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,8 +2,17 @@ import React from 'react'
 
 export default function Toolbar(props) {
     return (
-        <div>
-            
-        </div>
+        <header>
+            {/*These are placeholders*/}
+            <div>Menu</div>
+            <div>Logo</div>
+            <nav>
+                <ul>
+                    <li>One</li>
+                    <li>Two</li>
+                    <li>Three</li>
+                </ul>
+            </nav>
+        </header>
     )
 }
