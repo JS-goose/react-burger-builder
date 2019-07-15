3d0c658ee11175221d02d4b60feef622f4ad64f2
Jonathan Sexton <joncsexton@gmail.com>
2019-07-15 04:23:30 -0500

    ourtsourced nav items to their own component

---
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './NavigationItems.module.css';
+
+export default function NavigationItems() {
+  return (
+    <ul>
+      <li>
+        <a href="/">Home</a>
+      </li>
+    </ul>
+  );
+}
