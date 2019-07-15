1162e60f3795f1f510a94dd3f4eafeafa9355290
Jonathan Sexton <joncsexton@gmail.com>
2019-07-15 05:22:47 -0500

    adjusted rules and styling for active links

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -4,7 +4,7 @@ import styles from './NavigationItem.module.css';
 export default function NavigationItem(props) {
   return (
     <li className={styles['navigationItem']}>
-      <a href={props.link}>
+      <a href={props.link} className={props.active ? styles['active'] : null}>
         {props.children}
       </a>
     </li>
