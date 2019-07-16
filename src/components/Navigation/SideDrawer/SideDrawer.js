b784661c28613be4cf0334ed49b30a78af3c5fae
Jonathan Sexton <joncsexton@gmail.com>
2019-07-16 04:27:20 -0500

    imported styles

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import NavigationItems from '../NavigationItems/NavigationItems';
 import logo from '../../../assets/food-stand.svg';
+import styles from './SideDrawer.module.css';
 
 const SideDrawer = () => {
     return (
