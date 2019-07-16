833b4b710e22ab8913e3b62baeb55ff75233dc45
Jonathan Sexton <joncsexton@gmail.com>
2019-07-16 04:35:44 -0500

    imported sidedrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import Toolbar from '../Navigation/Toolbar/Toolbar'
-import styles from './Layout.module.css'
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+import styles from './Layout.module.css';
 
 const layout = (props) => {
   return (
     <React.Fragment>
       <Toolbar />
+      <SideDrawer />
       <main id={styles['content']}>{props.children}</main>
     </React.Fragment>
   );
