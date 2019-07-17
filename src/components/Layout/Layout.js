031e765788ad1904fd56c46e42fe7703c120d9dc
Jonathan Sexton <joncsexton@gmail.com>
2019-07-17 05:07:57 -0500

    converted Layout to class component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 import styles from './Layout.module.css';
 
-const layout = (props) => {
-  return (
-    <React.Fragment>
-      <Toolbar />
-      <SideDrawer />
-      <main id={styles['content']}>{props.children}</main>
-    </React.Fragment>
-  );
-};
+class Layout extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Toolbar />
+        <SideDrawer />
+        <main id={styles['content']}>{this.props.children}</main>
+      </React.Fragment>
+    );
+  }
+}
 
-export default layout;
+export default Layout;
