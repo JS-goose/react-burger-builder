207778e4cf9658cfcfb55bb4c6bdbd0e43ef6cce
Jonathan Sexton <joncsexton@gmail.com>
2019-07-17 05:02:29 -0500

    added backdrop to the sideDrawer

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,17 +1,21 @@
-import React from 'react'
+import React from 'react';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import Backdrop from '../../UI/Backdrop/Backdrop';
 import logo from '../../../assets/food-stand.svg';
 import styles from './SideDrawer.module.css';
 
 const SideDrawer = () => {
-    return (
-        <section className={styles.SideDrawerContainer}>
-           <img src={logo} alt="a food stand logo"/>
-           <nav className={styles.sideDrawerNav}>
-               <NavigationItems />
-           </nav>
-        </section>
-    )
-}
+  return (
+    <React.Fragment>
+      <Backdrop show/>
+      <section className={styles.SideDrawerContainer}>
+        <img src={logo} alt="a food stand logo" />
+        <nav className={styles.sideDrawerNav}>
+          <NavigationItems />
+        </nav>
+      </section>
+    </React.Fragment>
+  );
+};
 
-export default SideDrawer
+export default SideDrawer;
