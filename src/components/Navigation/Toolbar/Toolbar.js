763c85e17b05d4d6f6d50ef526d7bc036cfdd2b9
Jonathan Sexton <joncsexton@gmail.com>
2019-07-19 04:48:40 -0500

    imported MenuToggle and passed props

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import MenuToggle from '../SideDrawer/MenuToggle/MenuToggle';
 import styles from './Toolbar.module.css';
 // Icon from Darius Dan on https://flaticon.com
 import logo from '../../../assets/food-stand.svg';
@@ -9,7 +10,8 @@ export default function Toolbar(props) {
     <header className={styles['Toolbar']}>
       {/*These are placeholders*/}
       <img src={logo} alt="a food stand icon" />
-      <div onClick={props.closed}>Menu</div>
+      {/* <div onClick={props.closed}>Menu</div> */}
+      <MenuToggle toggleShow={props.closed} />
       <nav className={styles.showOnDesktop}>
         <NavigationItems />
       </nav>
